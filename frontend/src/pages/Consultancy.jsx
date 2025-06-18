524a092f0112f617b1e1d5ae6d53bd03b87dc513
Ambati Venkata Sai Tejesh <ambatikalyani1582@gmail.com>
2025-06-18 22:04:55 +0530

    Link not imported issue resolved

--- frontend/src/pages/Consultancy.jsx
+++ frontend/src/pages/Consultancy.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 import PageSkeleton from '../components/LoadingSkeleton/PageSkeleton';
 import {
   Typography,
