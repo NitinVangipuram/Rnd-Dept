2d52901b762aeca8e70a6ee2ef7a9fedd407a29b
bolloju <sreehb-123>
2025-10-18 14:50:54 +0530

    linked csr donations

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -66,13 +66,21 @@ const Navbar = ({ closeMenu }) => {
                <Ethics/>
 
                 <li className='rounded-lg hover:bg-slate-100 transition-all duration-200'>
-                    <NavLink
+                    {/* <NavLink
                         to="/csr"
                         className={({ isActive }) => `py-2 rounded-lg pl-3 font-medium block w-full ${isActive ? 'bg-slate-100 text-indigo-600' : 'text-gray-700'}`}
                         onClick={handleLinkClick}
                     >
                         CSR Donations
-                    </NavLink>
+                    </NavLink> */}
+                    <a
+                        href="https://iitdh.ac.in/csr-contribution"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='py-2 rounded-lg pl-3 font-medium block w-full text-gray-700 hover:bg-slate-100 transition-all duration-200'
+                    >
+                        CSR Donations
+                    </a>
                 </li>
                 
                 <li className='rounded-lg hover:bg-slate-100 transition-all duration-200'>
